62abd91c3c993a0d85d739a9be26d37ae11d308f
Jonathan Chong <jonathan@jonathan.local>
2018-08-27 11:06:20 -0700

    Bronze-Gold chapter 11 challenges completed without sections

--- Homepwner/Controller/ItemsViewController.swift
+++ Homepwner/Controller/ItemsViewController.swift
@@ -12,22 +12,172 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+    /*Button Stuff ~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        //Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        //Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+        //Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
     }
     
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        if isEditing {
+            //Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            
+            //Turn off editing mode
+            setEditing(false, animated: true)
+        }
+        else {
+            //Change text of button to inform user of state
+            sender.setTitle("Done", for:.normal)
+            
+            //Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Remove \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+                self.itemStore.removeItem(item)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+            ac.addAction(deleteAction)
+            
+            present(ac, animated: true, completion: nil)
+            
+            //Remove the item from the store
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt: IndexPath) -> String? {
+        return "Remove"
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print("move")
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count + 1
+    }
+    
+    /* No more items stuff and Sections!! ~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    
+    /*override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    
+    
+    //-----------There's an issue right now with uneven index when I'm inserting a new row. Need to find cause. Is it sections or is it an issue with the indexPath because No more items! messed it up?
+    
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Over $50"
+        default:
+            return "Others"
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return itemStore.highValueItems.count
+            
+        default:
+            return itemStore.otherItems.count + 1
+        }
+    }*/
+    
+    //prevent reordering silver challenge
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableViewCell, canEditRowAt indexPath: IndexPath) -> Bool {
+        if (indexPath.row >= itemStore.allItems.count) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if (proposedDestinationIndexPath.row >= itemStore.allItems.count) {
+            return sourceIndexPath
+        }
+        return proposedDestinationIndexPath
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        /*let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         let item = itemStore.allItems[indexPath.row]
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
         return cell
+        */
+        
+        //silver challenge test solution
+        print(indexPath.row)
+        print(itemStore.allItems.count)
+        if indexPath.row < itemStore.allItems.count {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+            let item = itemStore.allItems[indexPath.row]
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            return cell
+        }
+        else {
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell_NMI")
+            cell.textLabel?.text = "No more Items!"
+            return cell
+        }
+        
+        /*let items: [Item]
+        switch indexPath.section {
+        case 0:
+            items = itemStore.highValueItems
+        default:
+            items = itemStore.otherItems
+        }
+        
+        if indexPath.section == 1 && indexPath.row == items.count {
+            cell.textLabel?.text = "No more items!"
+            cell.detailTextLabel?.text = ""
+        } else {
+            let item = items[indexPath.row]
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "\(item.valueInDollars)"
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        }
+        cell.backgroundColor = UIColor.clear
+        return cell*/
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //Get height of the status bar
         let statusBarHeight = UIApplication.shared.statusBarFrame.height
         
--- Homepwner/Model/ItemStore.swift
+++ Homepwner/Model/ItemStore.swift
@@ -11,11 +11,13 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
+    /*var highValueItems: [Item] {
+        return allItems.filter{$0.valueInDollars > 50}
     }
+    
+    var otherItems: [Item] {
+        return allItems.filter{$0.valueInDollars <= 50}
+    }*/
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -23,6 +25,26 @@ class ItemStore {
         allItems.append(newItem)
         
         return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        //Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        //Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        //Insert item in array at new location
+        allItems.insert(movedItem, at:toIndex)
     }
     
 }
